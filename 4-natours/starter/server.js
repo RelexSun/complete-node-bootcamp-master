b02879d5b165f2ee8d46dfc41eb37c707545d043
RelexSun <relexsunnop@gmail.com>
2023-10-16 19:54:58 +0700

    sect 8

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,10 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({
   path: './.env',
 });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 // 4) START THE SERVER
 const port = process.env.PORT || 3000;
