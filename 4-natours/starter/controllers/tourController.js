6b4577467e8e78f9754bc80393d74a51ae9012b1
RelexSun <relexsunnop@gmail.com>
2023-10-19 19:33:13 +0700

    97. api:sorting

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,21 +2,48 @@ const Tour = require('./../models/tourModels');
 
 // This is middleware, used for cheking whether or not the tour data contains id, price, name and more.
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'error',
-      message: 'Name and price are required',
-    });
-  }
-  next();
-};
+// exports.checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: 'error',
+//       message: 'Name and price are required',
+//     });
+//   }
+//   next();
+// };
 
 // 2) ROUTE HANDLER refactore routes
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    console.log(req.query);
 
+    // Build query
+    // 1. Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 2. Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    const query = await Tour.find(JSON.parse(queryStr));
+
+    // { difficulty: 'easy', duration: { $gte: 5 } }
+    // { difficulty: 'easy', duration: { gte: '5' } }
+    // gte, gt, lte, lt
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // Execute query
+    const tours = await query;
+
+    // send response
     res.status(200).json({
       status: 'success',
       result: tours.length,
--- 4-natours/starter/dev-data/data/import-dev-data.js
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModels');
 
 dotenv.config({
   path: './.env',
@@ -9,6 +11,7 @@ const DB = process.env.DATABASE.replace(
   '<password>',
   process.env.DATABASE_PASSWORD,
 );
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -16,3 +19,38 @@ mongoose
     useFindAndModify: true,
   })
   .then(() => console.log('DB connection successful!'));
+
+// READ Json file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+// Import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE all data from DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] == '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
