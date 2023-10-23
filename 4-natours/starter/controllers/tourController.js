06d5ba1a84aaa44fab91e368f4aab90d6e3f6c45
RelexSun <88622289+RelexSun@users.noreply.github.com>
2023-10-23 11:43:03 +0700

    api pagination

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -25,19 +25,26 @@ exports.getAllTours = async (req, res) => {
 
     // 1B. Advanced filtering
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `${match}`);
 
     let query = await Tour.find(JSON.parse(queryStr));
 
     // 2 Sorting
     if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join('');
-      console.log(sortBy);
+      const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
       // sort ('price ratingsAverage')
+    } else {
+      query = query.sort('-createdAt');
     }
 
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
     // { difficulty: 'easy', duration: { $gte: 5 } }
     // { difficulty: 'easy', duration: { gte: '5' } }
     // gte, gt, lte, lt
@@ -47,6 +54,18 @@ exports.getAllTours = async (req, res) => {
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // page=2&limit=10, page 1, 1-10, page 2, 21-30 page 3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip > numTours) throw new Error('This page does not exist');
+    }
 
     // Execute query
     const tours = await query;
