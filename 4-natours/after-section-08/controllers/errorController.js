7ffb1812c3b5625e0ffd21f8e265a0abdd258d23
RelexSun <88622289+RelexSun@users.noreply.github.com>
2023-11-28 21:21:07 +0700

    Errors during development vs production

--- 4-natours/after-section-08/controllers/errorController.js
+++ 4-natours/after-section-08/controllers/errorController.js
@@ -1,3 +1,33 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send messafe to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.log('Error', err);
+
+    // 2) Send generic error message
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went wrong'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
 
@@ -5,8 +35,10 @@ module.exports = (err, req, res, next) => {
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
