ff0b03ec66d002a05c8a4bd774576042b57e4436
RelexSun <relexsunnop@gmail.com>
2023-11-29 13:57:27 +0700

    Handing Mongoose validation Errors

--- 4-natours/after-section-08/controllers/errorController.js
+++ 4-natours/after-section-08/controllers/errorController.js
@@ -13,6 +13,13 @@ const handleDuplicationFieldsDB = err => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('.')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -57,5 +64,9 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicationFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(err, res);
   }
 };
