e5a38641b45506100a5f37e2bc5b17904e4a7c32
RelexSun <relexsunnop@gmail.com>
2023-11-30 18:00:26 +0700

    hash password and jwt

--- 4-natours/after-section-09/models/userModel.js
+++ 4-natours/after-section-09/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const { Schema, model } = mongoose;
 
@@ -29,9 +30,23 @@ const userSchema = new Schema({
       validator: function(el) {
         return el === this.password;
         // to check weather password is match with passwordConfirm
-      }
+      },
+      message: 'Password are not the same'
     }
   }
+});
+
+// Pre middleware functions are executed one after another, when each middleware calls next.
+userSchema.pre('save', async function(next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // hash is asyncronous function and hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12); // 12 is a salt parameter to encrypt the password the higher it goes the more cpu intensive will be.
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined; // don't have to be in a database
+  next();
 });
 
 module.exports = model('User', userSchema);
