a7145481aaeb94802c23b112276581b414ee6290
RelexSun <relexsunnop@gmail.com>
2023-12-01 09:22:37 +0700

    fix role expose

--- 4-natours/after-section-09/controllers/authController.js
+++ 4-natours/after-section-09/controllers/authController.js
@@ -3,7 +3,13 @@ const catchAsync = require('../utils/catchAsync');
 
 // catchAsync is a custom utils for *try catch*
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // const newUser = await User.create(req.body); // will expose role
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
   res.status(201).json({
     status: 'success',
     data: {
