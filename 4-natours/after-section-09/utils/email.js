f4475f9cabfc423e746ddad69fa8cb7b219ab5e5
RelexSun <relexsunnop@gmail.com>
2023-12-07 14:04:06 +0700

    ...

--- 4-natours/after-section-09/utils/email.js
+++ 4-natours/after-section-09/utils/email.js
@@ -5,6 +5,7 @@ const sendEmail = async options => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD
@@ -12,7 +13,7 @@ const sendEmail = async options => {
   });
   // 2) Define the email options
   const mailOptions = {
-    from: 'Sun sun <sun@test.com>',
+    from: process.env.EMAIL_USERNAME,
     to: options.email,
     subject: options.subject,
     text: options.message
